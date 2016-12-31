23208730ee0a15a1641418d7d245a0b9a841e296
archy <hq_dev@163.com>
2016-12-31 21:21:02 +0800

    增加了 Java Validate API 相关注解和代码，对上传的 Spitter 实例属性进行验证

--- SpringInAction/src/main/java/com/archy/spittr/web/SpitterController.java
+++ SpringInAction/src/main/java/com/archy/spittr/web/SpitterController.java
@@ -4,9 +4,14 @@ import com.archy.spittr.Spitter;
 import com.archy.spittr.data.SpitterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 /**
   * @ClassName: SpitterController
@@ -34,9 +39,23 @@ public class SpitterController {
     }
 
     @RequestMapping(value = "/register", method = RequestMethod.POST)
-    public String processRegistration(Spitter spitter) {
+    public String processRegistration(@Valid  Spitter spitter, Errors errors) {
+
+        if (errors.hasErrors()) {
+            return "registerForm";
+        }
         spitterRepository.save(spitter);
 
         return "redirect:/spitter/" + spitter.getUsername();
     }
+
+    @RequestMapping(value = "/{username}", method = RequestMethod.GET)
+    public String showSpitterProfile(
+            @PathVariable String username, Model model
+    ) {
+        Spitter spitter = spitterRepository.findByUsername(username);
+        model.addAttribute(spitter);
+        return "profile";
+    }
+
 }
